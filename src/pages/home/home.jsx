d1d0104b39fea214be87d7995e0b2dc5f847fe75
ShariyarAbeer <007abeershariyar@gmail.com>
2020-02-15 05:32:37 +0600

    update

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -29,31 +29,31 @@ class Home extends Component {
               </div>
               <Menu.Item key="1">
                 <Icon type="video-camera" />
-                <span className="nav-text">nav 2</span>
+                <span className="nav-text">nav 1</span>
               </Menu.Item>
               <Menu.Item key="2">
                 <Icon type="upload" />
-                <span className="nav-text">nav 3</span>
+                <span className="nav-text">nav 2</span>
               </Menu.Item>
               <Menu.Item key="3">
                 <Icon type="bar-chart" />
-                <span className="nav-text">nav 4</span>
+                <span className="nav-text">nav 3</span>
               </Menu.Item>
               <Menu.Item key="4">
                 <Icon type="cloud-o" />
-                <span className="nav-text">nav 5</span>
+                <span className="nav-text">nav 4</span>
               </Menu.Item>
               <Menu.Item key="5">
                 <Icon type="appstore-o" />
-                <span className="nav-text">nav 6</span>
+                <span className="nav-text">nav 5</span>
               </Menu.Item>
               <Menu.Item key="6">
                 <Icon type="team" />
-                <span className="nav-text">nav 7</span>
+                <span className="nav-text">nav 6</span>
               </Menu.Item>
               <Menu.Item key="7">
                 <Icon type="shop" />
-                <span className="nav-text">nav 8</span>
+                <span className="nav-text">nav 7</span>
               </Menu.Item>
             </Menu>
           </Sider>
