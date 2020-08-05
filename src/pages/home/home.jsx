de4e764559433f137dafc34f2f5a937a23d9a69a
EyePatch <007abeershariyar@gmail.com>
2020-08-06 03:51:39 +0600

    new update

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,15 +1,73 @@
 import React, { Component } from "react";
-import { Layout, Menu, Icon, Avatar, Card } from "antd";
 import "antd/dist/antd.css";
-
+import { Layout, Menu } from "antd";
+import {
+	UploadOutlined,
+	UserOutlined,
+	VideoCameraOutlined,
+} from "@ant-design/icons";
+import "./home.css";
 const { Header, Content, Footer, Sider } = Layout;
 
 class Home extends Component {
-  state = {};
-  render() {
-    return (
-      <div>
-        <Layout>
+	state = {};
+
+	render() {
+		return (
+			<div>
+				<Layout>
+					<Sider
+						breakpoint="lg"
+						collapsedWidth="0"
+						onBreakpoint={(broken) => {
+							console.log(broken);
+						}}
+						onCollapse={(collapsed, type) => {
+							console.log(collapsed, type);
+						}}
+					>
+						<div className="logo" />
+
+						<Menu
+							style={{ marginTop: 80 }}
+							theme="dark"
+							mode="inline"
+							defaultSelectedKeys={["2"]}
+						>
+							<Menu.Item key="1" icon={<UserOutlined />}>
+								nav 1
+							</Menu.Item>
+							<Menu.Item key="2" icon={<VideoCameraOutlined />}>
+								nav 2
+							</Menu.Item>
+							<Menu.Item key="3" icon={<UploadOutlined />}>
+								nav 3
+							</Menu.Item>
+							<Menu.Item key="4" icon={<UserOutlined />}>
+								nav 4
+							</Menu.Item>
+						</Menu>
+					</Sider>
+					<Layout>
+						<Header
+							className="site-layout-sub-header-background"
+							style={{ padding: 0 }}
+						/>
+						<Content style={{ margin: "24px 16px 0" }}>
+							<div
+								className="site-layout-background"
+								style={{ padding: 24, minHeight: 360 }}
+							>
+								content
+							</div>
+						</Content>
+						<Footer style={{ textAlign: "center" }}>
+							Ant Design ©2018 Created by Abeer
+						</Footer>
+					</Layout>
+				</Layout>
+				,
+				{/* <Layout>
           <Sider
             style={{
               overflow: "auto",
@@ -72,10 +130,10 @@ class Home extends Component {
               Design ©2020 Created by Md.Arif Shariyar
             </Footer>
           </Layout>
-        </Layout>
-      </div>
-    );
-  }
+        </Layout> */}
+			</div>
+		);
+	}
 }
 
 export default Home;
