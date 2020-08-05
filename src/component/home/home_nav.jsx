a46dd6a375186ff6448656be2b135666cc93a8ed
EyePatch <007abeershariyar@gmail.com>
2020-08-06 04:02:01 +0600

    nav update

---
+++ src/component/home/home_nav.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class HomeNav extends Component {
+	state = {};
+	render() {
+		return (
+			<div>
+				<h1>Home_Nav</h1>
+			</div>
+		);
+	}
+}
+
+export default HomeNav;
--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -35,16 +35,16 @@ class Home extends Component {
 							defaultSelectedKeys={["2"]}
 						>
 							<Menu.Item key="1" icon={<UserOutlined />}>
-								nav 1
+								Home
 							</Menu.Item>
 							<Menu.Item key="2" icon={<VideoCameraOutlined />}>
-								nav 2
+								Profile
 							</Menu.Item>
 							<Menu.Item key="3" icon={<UploadOutlined />}>
-								nav 3
+								About
 							</Menu.Item>
 							<Menu.Item key="4" icon={<UserOutlined />}>
-								nav 4
+								Work
 							</Menu.Item>
 						</Menu>
 					</Sider>
@@ -66,71 +66,6 @@ class Home extends Component {
 						</Footer>
 					</Layout>
 				</Layout>
-				,
-				{/* <Layout>
-          <Sider
-            style={{
-              overflow: "auto",
-              height: "100vh",
-              position: "auto",
-              left: 0
-            }}
-          >
-            <div className="logo" />
-            <Menu theme="dark" mode="inline" defaultSelectedKeys={["1"]}>
-              <div theme="dark">
-                <Avatar
-                  style={{ margin: 40 }}
-                  size={100}
-                  src="https://avatars1.githubusercontent.com/u/34870297?s=400&u=4e2aa1397f994a283acfa72d5b69a67621e7c500&v=4"
-                />
-              </div>
-              <Menu.Item key="1">
-                <Icon type="video-camera" />
-                <span className="nav-text">nav 1 </span>
-              </Menu.Item>
-              <Menu.Item key="2">
-                <Icon type="upload" />
-                <span className="nav-text">nav 2</span>
-              </Menu.Item>
-              <Menu.Item key="3">
-                <Icon type="bar-chart" />
-                <span className="nav-text">nav 3</span>
-              </Menu.Item>
-              <Menu.Item key="4">
-                <Icon type="cloud-o" />
-                <span className="nav-text">nav 4</span>
-              </Menu.Item>
-              <Menu.Item key="5">
-                <Icon type="appstore-o" />
-                <span className="nav-text">nav 5</span>
-              </Menu.Item>
-              <Menu.Item key="6">
-                <Icon type="team" />
-                <span className="nav-text">nav 6</span>
-              </Menu.Item>
-              <Menu.Item key="7">
-                <Icon type="shop" />
-                <span className="nav-text">nav 7</span>
-              </Menu.Item>
-            </Menu>
-          </Sider>
-          <Layout style={{ marginLeft: 200 }}>
-            <Header style={{ background: "#fff", padding: 0 }} />
-            <Content style={{ margin: "24px 16px 0", overflow: "initial" }}>
-              <div
-                style={{ padding: 24, background: "#fff", textAlign: "center" }}
-              >
-                ...
-                <br />
-                Really content
-              </div>
-            </Content>
-            <Footer style={{ textAlign: "center" }}>
-              Design ©2020 Created by Md.Arif Shariyar
-            </Footer>
-          </Layout>
-        </Layout> */}
 			</div>
 		);
 	}
